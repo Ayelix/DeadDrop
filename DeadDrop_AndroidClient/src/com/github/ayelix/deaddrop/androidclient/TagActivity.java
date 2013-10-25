c90ba14e14cb1e7eefdb166d10f17fe3712899c2
Alex Mosolgo <amosolgo@gmail.com>
2013-10-25 13:36:39 -0400

    Updated strings to use resources, resolved several warnings.

--- DeadDrop_AndroidClient/src/com/github/ayelix/deaddrop/androidclient/TagActivity.java
+++ DeadDrop_AndroidClient/src/com/github/ayelix/deaddrop/androidclient/TagActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
