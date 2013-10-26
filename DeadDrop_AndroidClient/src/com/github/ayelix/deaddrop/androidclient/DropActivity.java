4eeabe29859d682b7c9edf29f24f97ed0d084ae0
Alex Mosolgo <amosolgo@gmail.com>
2013-10-25 20:22:52 -0400

    Added UI element references to DropActivity.

--- DeadDrop_AndroidClient/src/com/github/ayelix/deaddrop/androidclient/DropActivity.java
+++ DeadDrop_AndroidClient/src/com/github/ayelix/deaddrop/androidclient/DropActivity.java
@@ -5,14 +5,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DropActivity extends Activity {
 	public static final String TAG = "DropActivity";
+
+	private TextView m_dataTextView;
+	private EditText m_dataEditText;
+	private TextView m_accuracyTextView;
+	private EditText m_accuracyEditText;
+	private Button m_imageButton;
+	private ImageView m_imageView;
+	private Button m_dropButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_drop);
+
+		// Get the views
+		m_dataTextView = (TextView) findViewById(R.id.dataTextView);
+		m_dataEditText = (EditText) findViewById(R.id.dataEditText);
+		m_accuracyTextView = (TextView) findViewById(R.id.accuracyTextView);
+		m_accuracyEditText = (EditText) findViewById(R.id.accuracyEditText);
+		m_imageButton = (Button) findViewById(R.id.imageButton);
+		m_imageView = (ImageView) findViewById(R.id.imageView);
+		m_dropButton = (Button) findViewById(R.id.dropButton);
 
 		// Get the intent that started the activity
 		Intent intent = getIntent();
